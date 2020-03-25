5af9776eaa072743bc27404b70da51e7408bb44b
julia servetnik <juliaservetnik2019@gmail.com>
2020-03-25 10:31:31 +0300

    test1

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("     1 What is this life if, full of care");
+        System.out.println("     2 We have no time to stand and stare,");
+        System.out.println("     3 No time to stand beneath the boughs,");
+        System.out.println("     4 And stare as long as sheep or cows.");
+    }
+}
