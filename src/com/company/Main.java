fce551e1871227bc4def58d58d579c3b4ba7e693
julia servetnik <juliaservetnik2019@gmail.com>
2020-03-25 15:06:26 +0300

    one more

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("     1 What is this life if, full of care");
-        System.out.println("     2 We have no time to stand and stare,");
-        System.out.println("     3 No time to stand beneath the boughs,");
-        System.out.println("     4 And stare as long as sheep or cows.");
+        double p = 3.14;
+        int r = 42;
+        double sum = p*Math.pow(r,2);
+        System.out.println(sum);
     }
 }
