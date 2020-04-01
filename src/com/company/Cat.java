e24cb8d42fc0416dd4c8ceb7ad1ee2c1bfa2a720
julia servetnik <juliaservetnik2019@gmail.com>
2020-04-01 16:34:36 +0300

    HW3 Cat

---
+++ src/com/company/Cat.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Cat{
+
+    int catAge;
+    String name;
+
+    //конструктор
+    public Cat(String name, int catAge){
+       this.catAge = catAge;
+       this.name = name;
+    }
+    //новый кот
+    public Cat(){
+    }
+}
---
+++ src/com/company/HW3.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class HW3 {
+    public static void main(String[] args) {
+        Cat simon = new Cat("Simon", 4);
+        System.out.println("name: " + simon.name);
+        System.out.println("age: " + simon.catAge);
+    }
+}
