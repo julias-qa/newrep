b3ec9a3ad583114c0279dc06dd2865df4fb65918
julia servetnik <juliaservetnik2019@gmail.com>
2020-04-07 10:24:41 +0300

    Bond Life

---
+++ src/com/company/Bond2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.animals.Bond;
+
+public class Bond2 {
+    public static void main(String[] args){
+        Bond myCat = new Bond();
+        myCat.eat();
+        myCat.food();
+        myCat.play(true);
+    }
+}
---
+++ src/com/company/Bond3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.animals.Bond;
+
+public class Bond3 {
+    public static void main(String[] args){
+        Bond myCat = new Bond();
+        myCat.eat();
+        myCat.nofood();
+        myCat.play(false);
+    }
+}
