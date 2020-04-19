c78c03e3ab45fbf5462762fdd8bd67c065ace113
julias-qa <LalaBla!2020>
2020-04-19 12:34:46 +0300

    collection commit 3 - improve

--- src/com/company/collection/businessTrip.java
+++ src/com/company/collection/businessTrip.java
@@ -1,20 +1,20 @@
-import java.util.ArrayList; // импортируется интерфейс ArrayList для возможности создания списков
-import java.util.Iterator; // импортируется интерфейс Iterator для возможности перебирать список
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class businessTrip {                                                      //создается класс с определенным названием
-    public static void main(String[] args)                                       //определяется метод
+public class businessTrip {
+    public static void main(String[] args)
     {
-        ArrayList bag = new ArrayList();                                        //создается объект с именем "bag"
-        bag.add("Зубная щетка");                                                //методом add объявили один предмет и далее до конца списка
+        ArrayList<String> bag = new ArrayList<>();
+        bag.add("Зубная щетка");
         bag.add("Блокнот");
         bag.add("Ручка");
         bag.add("Галстук");
         bag.add("Словарь");
         bag.add("Носки");
 
-        Iterator trip = bag.iterator();                                         //создаем объект с именем trip
-        while(trip.hasNext()){                                                  //через цикл while задаем условие вывода списка + hasNext() узнает, есть ли следующий элемент - т.е. пока есть следующий элемент, список будет выводиться
-            System.out.println(trip.next());                                    //выводим результат, используя метод next после hasNext, чтобы не было ошибки "NoSuchElementException"
+        Iterator trip = bag.iterator();
+        for (String str:bag){
+            System.out.println(str);
         }
     }
 }
